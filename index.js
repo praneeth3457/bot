4a49474fc4a5f4b09f4adb4518ec62ce3b018691
praneeth <praneeth7516@gmail.com>
2017-11-14 18:47:20 -0800

    fixed profile type

--- index.js
+++ index.js
@@ -11,31 +11,31 @@ app.get('/', function(req,res){
     res.send("Hello Bot!");
 });
 
-// // Creates the endpoint for our webhook
-// app.post('/webhook/', function(req, res) {
-//
-//     var body = req.body;
-//
-//     // Checks this is an event from a page subscription
-//     if (body.object === 'page') {
-//
-//         // Iterates over each entry - there may be multiple if batched
-//         body.entry.forEach(function(entry) {
-//
-//             // Gets the message. entry.messaging is an array, but
-//             // will only ever contain one message, so we get index 0
-//             var webhookEvent = entry.messaging[0];
-//             console.log(webhookEvent);
-//         });
-//
-//         // Returns a '200 OK' response to all requests
-//         res.status(200).send('EVENT_RECEIVED');
-//     } else {
-//         // Returns a '404 Not Found' if event is not from a page subscription
-//         res.sendStatus(404);
-//     }
-//
-// });
+// Creates the endpoint for our webhook
+app.post('/webhook/', function(req, res) {
+
+    var body = req.body;
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            var webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+
+});
 
 // Adds support for GET requests to our webhook
 app.get('/webhook/', function(req, res) {
